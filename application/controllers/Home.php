fb78e1770cb004e2676aa8320d1da93202471a4d
MohannadNaj <me@mohannad.io>
2016-09-22 04:04:51 +0300

    create Home controller & view

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -4,6 +4,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Home extends CI_Controller {
 	public function index()
 	{
-		
+		$data['main_content'] = "home";
+		$this->load->view('layout', $data);
 	}
 }
