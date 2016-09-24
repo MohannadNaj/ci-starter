4c8f3f3b7bdb0abce10a4d8a9c2b514d4ba96852
MohannadNaj <me@mohannad.io>
2016-09-24 13:57:03 +0300

    allow css sourcemaps.

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@ const elixir = require('laravel-elixir');
 
 require('laravel-elixir-vue');
 
-elixir.config.sourcemaps = false;
+elixir.config.sourcemaps = true;
 elixir.config.appPath = 'application';
 elixir.config.viewPath = 'application/views';
 
