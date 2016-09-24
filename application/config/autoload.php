3472fafe62ffc0a812d019d525569acea3ec91d8
MohannadNaj <me@mohannad.io>
2016-09-24 13:49:32 +0300

    custom config file

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -103,7 +103,7 @@ $autoload['helper'] = array('php_string_functions','url','global');
 | config files.  Otherwise, leave it blank.
 |
 */
-$autoload['config'] = array();
+$autoload['config'] = array('app');
 
 /*
 | -------------------------------------------------------------------
