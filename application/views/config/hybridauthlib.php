3709b2894b45f2b4c78d1da9adcf309a36feed31
MohannadNaj <me@mohannad.io>
2016-09-24 14:06:57 +0300

    social buttons: separated view-logic file.

---
+++ application/views/config/hybridauthlib.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+	'Twitter' => [
+		'label' => 'Twitter',
+		'font_awesome' => 'fa-twitter',
+		'btn_class' => 'btn-twitter'
+	],
+	'Google' => [
+		'label' => 'Google',
+		'font_awesome' => 'fa-google-plus',
+		'btn_class' => 'btn-google'
+	]
+];
