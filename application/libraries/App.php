a17fc3d08956e9d9238b86cd91bba107d1c3240e
MohannadNaj <me@mohannad.io>
2016-09-29 01:27:49 +0300

    load some helper variables to all views.

--- application/libraries/App.php
+++ application/libraries/App.php
@@ -6,14 +6,17 @@ class App
 	function __construct($config = array())
 	{
 		$this->ci =& get_instance();
-		$data['providers'] = $this->ci->hybridauthlib->getProviders();
-		$data['auth_view'] =  ($this->ci->ion_auth->logged_in() ? "user" : "guest");
-		$this->ci->load->vars($data);
 		$this->init();
 	}
 
 	public function init()
 	{
+		$data['providers'] = $this->ci->hybridauthlib->getProviders();
+		$data['auth_view'] =  ($this->ci->ion_auth->logged_in() ? "user" : "guest");
+		$data['is_user'] = $this->ci->ion_auth->logged_in() ;
+		$data['is_password_by_social'] = $this->ci->session->userdata('user')['is_password_by_social'];
+
+		$this->ci->load->vars($data);
 	}
 }
 
