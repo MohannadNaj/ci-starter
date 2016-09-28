e708114bb0aec3bc8dd79edf19698eed53ce11d2
MohannadNaj <me@mohannad.io>
2016-09-28 22:29:44 +0300

    loader constructor

--- application/core/MY_Loader.php
+++ application/core/MY_Loader.php
@@ -5,6 +5,8 @@ class MY_Loader extends CI_Loader {
     public function __construct() {
         $this->_ci_view_paths = array(
             VIEWSDIR => TRUE
-        );        
+        );
+        parent::__construct();
     }
+
 }
