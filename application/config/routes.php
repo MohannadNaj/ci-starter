22dcffc0616e401ca5009e3afd7393aa4d5dfb27
MohannadNaj <me@mohannad.io>
2016-10-03 21:38:28 +0300

    config: admin and user profiles routes

--- application/config/routes.php
+++ application/config/routes.php
@@ -52,3 +52,5 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 $route['default_controller'] = 'home';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
+$route['admin'] = 'Auth/index';
+$route['u/(:any)'] = 'User/profile/$1';
