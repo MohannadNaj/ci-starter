95ad0255b4c386ade60dbc251611e9617d66f474
MohannadNaj <me@mohannad.io>
2016-10-08 02:10:03 +0300

    move validation rules to controllers dir (lower-case class name as prefix)

--- application/libraries/MY_Form_validation.php
+++ application/libraries/MY_Form_validation.php
@@ -17,18 +17,16 @@ class MY_Form_validation extends CI_Form_validation
 	public function load($path = false)
 	{
 		if(! $path)
-			$path = str_ireplace('/', DIRECTORY_SEPARATOR, __DIR__ . '/Validation/' . $this->CI->router->directory . $this->CI->router->class . '/' . $this->CI->router->method);
+			$path = str_ireplace('/', DIRECTORY_SEPARATOR, APPPATH . 'controllers/validation/' .  $this->CI->router->directory . $this->CI->router->class . '/' . $this->CI->router->method);
 
 		if(!endsWith($path, '.php') && !contains($path, '.')) {
 			$path .= '.php';
 		}
 		
-
 		if(! file_exists($path)) {
 			$guesses = array( 
-				str_ireplace('/', DIRECTORY_SEPARATOR, __DIR__ . '/Validation/' . $this->CI->router->directory . $this->CI->router->class . '/' . $path),
-				str_ireplace('/', DIRECTORY_SEPARATOR, __DIR__ . '/Validation/' . $path),
-				str_ireplace('/', DIRECTORY_SEPARATOR, __DIR__ . '/' . $path)
+				str_ireplace('/', DIRECTORY_SEPARATOR, APPPATH . 'controllers/validation/' . $this->CI->router->directory . $this->CI->router->class . '/' . $path),
+				str_ireplace('/', DIRECTORY_SEPARATOR, APPPATH . 'controllers/validation/' . $path),
 				);
 			foreach ($guesses as $guess_path) {
 				if(file_exists($guess_path)) {
