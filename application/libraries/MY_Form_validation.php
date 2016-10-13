d32b89499c7d0b9a8aefe8eed5210da231e402f3
MohannadNaj <me@mohannad.io>
2016-10-13 04:57:24 +0300

    MY_Form_validation: refactor, private functions for sub-tasks

--- application/libraries/MY_Form_validation.php
+++ application/libraries/MY_Form_validation.php
@@ -16,44 +16,63 @@ class MY_Form_validation extends CI_Form_validation
 
 	public function load($path = false)
 	{
-		if(! $path)
+		$path = $this->__get_path($path);
+		if($path) {
+			
+			$file = $this->__load($path);
+
+			if(is_array($file)) {
+				$this->set_rules($file);
+			}
+
+			return $this;
+		} else {
+			show_error('Validation file is not found');
+		}
+	}
+
+	private function __get_path($path = false) {
+		$found = false;
+
+		if( ! $path)
 			$path = str_ireplace('/', DIRECTORY_SEPARATOR, APPPATH . 'controllers/validation/' .  $this->CI->router->directory . $this->CI->router->class . '/' . $this->CI->router->method);
 
-		if(!endsWith($path, '.php') && !contains($path, '.')) {
+		if( ! endsWith($path, '.php') && !contains($path, '.')) {
 			$path .= '.php';
 		}
 		
-		if(! file_exists($path)) {
+		if( ! file_exists($path)) {
 			$guesses = array( 
 				str_ireplace('/', DIRECTORY_SEPARATOR, APPPATH . 'controllers/validation/' . $this->CI->router->directory . $this->CI->router->class . '/' . $path),
 				str_ireplace('/', DIRECTORY_SEPARATOR, APPPATH . 'controllers/validation/' . $path),
 				);
 			foreach ($guesses as $guess_path) {
 				if(file_exists($guess_path)) {
-					return $this->__load($guess_path);
+					$path = $guess_path;
+					$found = true;
+					break;
 				}
 			}
 		} else {
-			return $this->__load($path);
-			
+			$found = true;
 		}
 
-		show_error('Validation file is not found');
+		if($found) 
+			return $path;
+		else
+			return false;
 	}
 
 	private function __load($path) {
 		$ci = $this->CI;
 		
-		if(!empty($this->with)) {
+		if( ! empty($this->with)) {
 			extract($this->with);
 			$this->with = array();
 		}
 
 		$file = include $path;
 
-		if(is_array($file)) {
-			$this->set_rules($file);
-		}
-		return $this;
+		return $file;
 	}
 }
